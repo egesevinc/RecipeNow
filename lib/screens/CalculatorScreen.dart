f9725c0ff829d0be016a6cf957b1cd170d4ee3bc
Ege Sevinç <egesevinc@windowslive.com>
2023-01-19 22:17:27 +0300

    final

--- lib/screens/CalculatorScreen.dart
+++ lib/screens/CalculatorScreen.dart
@@ -1,25 +1,139 @@
 import 'package:flutter/material.dart';
-import 'package:se380_project/models/recipe_model.dart';
-import 'package:se380_project/models/recipe_search_result.dart';
+import 'dart:convert';
 import 'package:se380_project/screens/CalorieCalculatorScreen.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class CalculatorScreen extends StatefulWidget {
-  List<Recipetest> selectedfoods;
+  final List<dynamic> selectedFoods;
 
-  CalculatorScreen({ required this.selectedfoods});
+  CalculatorScreen({required this.selectedFoods});
 
   @override
   _CalculatorScreenState createState() => _CalculatorScreenState();
 }
+
 class _CalculatorScreenState extends State<CalculatorScreen> {
+  List<double> _amounts = [];
+  @override
+  void initState() {
+    super.initState();
+    if(widget.selectedFoods.length > 0) _amounts = List.generate(widget.selectedFoods.length, (_) => 100);
+  }
+
+  int _getTotalCalories() {
+    double total = 0;
+    if (widget.selectedFoods.length > 0) {
+      final calories = _amounts.asMap().entries.map((e) => e.value /100 *
+          widget.selectedFoods[e.key]["Column5"]).toList();
+      total = calories.fold(0, (sum, element) => sum + element);
+    }
+    return total.round();
+  }
 
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        appBar: AppBar(title: Text("Calorie Calculator")),
+        body:
+        Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: widget.selectedFoods.length,
+                  itemBuilder: (context,index) {
+                    final food = widget.selectedFoods[index];
+                    return ListTile(
+                      title: Text(food["Column2"]),
+                      subtitle: Text("Calories: ${food["Column5"] * _amounts[index] / 100}"),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            width: 150,
+                            child: Slider(
+                              value: _amounts[index],
+                              onChanged: (double newValue) {
+                                setState(() {
+                                  _amounts[index] = newValue;
+                                });
+                              },
+                              min: 0,
+                              max: 1000,
+                              divisions: 100,
+                              label: '${_amounts[index].round()} ',
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          Text('${_amounts[index].round()} g'),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              setState(() {
+                                widget.selectedFoods.removeAt(index);
+                                _amounts.removeAt(index);
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text(food["Column2"]),
+                              content: Text("\nCategory: ${food["Column4"]} \nProtein: ${food["Column6"]* _amounts[index].round() / 100} g \nCarbohydrate: ${food["Column7"]* _amounts[index].round() / 100} g \nFat: ${food["Column10"]* _amounts[index].round() / 100} g\nSugar: ${food["Column8"]* _amounts[index].round() / 100} g\nCalcium: ${food["Column38"]* _amounts[index].round() / 100} mg \nIron: ${food["Column41"]* _amounts[index].round() / 100} mg \nMagnesium: ${food["Column40"]* _amounts[index].round() / 100} mg \nPhosphorus: ${food["Column39"]* _amounts[index].round() / 100} mg \nPotassium: ${food["Column45"]* _amounts[index].round() / 100} mg \nSodium: ${food["Column46"]* _amounts[index].round() / 100} mg \nZinc: ${food["Column42"]* _amounts[index].round() / 100} mg \nCholesterol: ${food["Column39"]* _amounts[index].round() / 100} mg \nAlcohol: ${food["Column49"]* _amounts[index].round() / 100} g"),
+                              actions: [
+                                TextButton(
+                                  child: Text("Close"),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
 
-      ),
+                    );
+
+                  }
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("Click on foods for the nutrient details",
+                  style: TextStyle(fontSize: 16, color: Colors.grey),),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                alignment: Alignment.center,
+                color: Colors.greenAccent,
+                child: Text(
+                  "Total Calories: ${_getTotalCalories()}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                  ),
+                ),
+              ),
+            ),
+
+          ],
+        )
+
+
     );
   }
 }
--- lib/screens/CalorieCalculatorScreen.dart
+++ lib/screens/CalorieCalculatorScreen.dart
@@ -1,98 +1,94 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
-import 'package:se380_project/services/api_service.dart' as ApiService;
-import 'package:se380_project/models/NutritionInfo.dart';
+import "package:flutter/material.dart";
+import 'dart:convert';
+
 import 'package:se380_project/screens/CalculatorScreen.dart';
-import '../models/recipe_search_result.dart';
-import '../services/api_service.dart';
-//import 'package:se380_project//widgets/food_list.dart';
 
 class CalorieCalculatorScreen extends StatefulWidget {
+  const CalorieCalculatorScreen({Key? key}) : super(key: key);
+
   @override
-  _CalorieCalculatorScreenState createState() => _CalorieCalculatorScreenState();
+  State<CalorieCalculatorScreen> createState() => _CalorieCalculatorScreenState();
 }
 
 class _CalorieCalculatorScreenState extends State<CalorieCalculatorScreen> {
-  final TextEditingController _foodController = TextEditingController();
-  List<Recipetest> _selectedfoods = [];
-
-  List _recipes = [];
-
-
-  Future<void> _recipeCalorie() async {
-    final recipeCalories = await APIService.instance.getNutritionInfo(food : _foodController.text);
-    print(NutritionInfo);
-    setState(() {
+  List<dynamic> _foods = [];
+  String _searchQuery = "";
+  List<dynamic> _selectedFoods = [];
+  @override
+  void initState() {
+    super.initState();
+    DefaultAssetBundle.of(context).loadString("assets/Foods.json")
+        .then((data){
+      _foods = jsonDecode(data);
     });
   }
 
+  List<dynamic> _getFilteredFoods() {
+    if (_searchQuery.isEmpty) {
+      return _foods;
+    }
+    return _foods.where((food) {
+      return food["Column2"].toLowerCase().contains(_searchQuery.toLowerCase());
+    }).toList();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      appBar: AppBar( title: Text("Calorie Calculator"),),
       body: Column(
         children: [
-          TextField(
-            controller: _foodController,
-            decoration: InputDecoration(
-              hintText: 'Enter food name',
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  _searchQuery=value;
+                });
+              },
+              decoration: InputDecoration(
+                  hintText: "Search for a food"
+              ),
             ),
           ),
-          ElevatedButton(
-            onPressed: _recipeCalorie,
-            child: Text('Search'),
-          ),
           Expanded(
-            child: FutureBuilder(
-              future: APIService.instance.searchRecipesByIngredient(
-                ingredient: _foodController.text,
-              ),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: snapshot.data?.length,
-                    itemBuilder: (context, index) {
-                      Recipetest recipe = snapshot.data![index];
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _selectedfoods.add(recipe);
-                          });
-                          // Navigate to the recipe screen
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => CalculatorScreen(
-                                selectedfoods: _selectedfoods,
-                              ),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          padding: EdgeInsets.all(3),
-                          child: ListTile(
-                            title: Text(recipe.title),
-                            leading: Image.network(recipe.imageUrl),
-                          ),
-                        ),
-                      );
-                      return ListTile(
-                        title: Text(recipe.title),
-                        leading: Image.network(recipe.imageUrl),
-                      );
-                    },
-                  );
-                } else if (snapshot.hasError) {
-                  return Text(snapshot.error.toString());
-                }
-                return Center(
-                  child: CircularProgressIndicator(),
+            child: ListView.builder(
+              itemCount: _getFilteredFoods().length,
+              itemBuilder: (context,index) {
+                final food = _getFilteredFoods()[index];
+                return CheckboxListTile(
+                  value: _selectedFoods.contains(food),
+                  onChanged: (newValue){
+                    setState(() {
+                      if (newValue== true){
+                        _selectedFoods.add(food);
+                      } else {
+                        _selectedFoods.remove(food);
+                      }
+                    });
+                  },
+                  title: Text(food["Column2"]),
+
                 );
               },
+
+
             ),
           ),
         ],
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => CalculatorScreen(selectedFoods: _selectedFoods),),
+          );
+        },
+        child: Icon(Icons.check),
+        backgroundColor: Colors.green,
       ),
     );
   }
